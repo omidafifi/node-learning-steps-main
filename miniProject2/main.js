03d89f73ad8e29a7829703ffd0058ef99b69614f
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-18 11:00:56 +0330

    Wiederholung der Aufgaben nochmal

--- miniProject2/main.js
+++ miniProject2/main.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-fs.readFile("inventory.txt", "utf-8", (err, data) => {
-  if (err) {
-    throw err;
+fs.readFile("inventory.txt", "utf-8", (error, data) => {
+  if (error) {
+    throw error;
   }
   const db = JSON.parse(data);
-  const newBook = { id: 4, title: "Masanbavi" };
+  const newBook = { id: 4, title: "Masnavi" };
   db.Books.push(newBook);
   console.log(db);
   fs.writeFile("inventory.txt", JSON.stringify(db), (err) => {
