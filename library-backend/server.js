7192f3602d31af3d6e4b2868f3a105fb4e20b368
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-19 16:04:38 +0330

    +

--- library-backend/server.js
+++ library-backend/server.js
@@ -1,2 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === `/api/users`) {
+    fs.readFile("db.json", "utf-8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const db = JSON.parse(data.users);
+      console.log(db);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server runing on port 3000");
+});
