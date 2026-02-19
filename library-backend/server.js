975814471ed30b24a4d47354be031d87b217222d
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-19 17:13:44 +0330

    parsedDb

--- library-backend/server.js
+++ library-backend/server.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === `/api/users`) {
-    fs.readFile("db.json", "utf-8", (err, data) => {
+    fs.readFile("db.json", "utf-8", (err, db) => {
       if (err) {
         throw err;
       }
-      const db = JSON.parse(data.users);
-      console.log(db);
+
+      const parsedDb = JSON.parse(db);
+      console.log(parsedDb.users);
     });
   }
 });
