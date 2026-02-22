4ff13f0ecab34caf389d4d1e759ca33ebc343a8b
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-22 15:34:41 +0330

    get users and books api

--- library-backend/server.js
+++ library-backend/server.js
@@ -12,6 +12,16 @@ const server = http.createServer((req, res) => {
       res.write(JSON.stringify(data.users));
       res.end();
     });
+  } else if (req.method === "GET" && req.url === `/api/books`) {
+    fs.readFile("db.json", "utf-8", (err, db) => {
+      if (err) {
+        throw err;
+      }
+      const data = JSON.parse(db);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(data.books));
+      res.end();
+    });
   }
 });
 
