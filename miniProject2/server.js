4c2576ebbe266eb8da1e5f9c5bc461aa5840d47c
omid <111409640+omidafifi@users.noreply.github.com>
2026-02-24 19:35:27 +0330

    bist heute

--- miniProject2/server.js
+++ miniProject2/server.js
@@ -1,8 +1,16 @@
 const http = require("http");
-const { stringify } = require("querystring");
 const PORT = 3000;
+const products = require("./products");
 const server = http
-  .createServer((req, res) => {})
+  .createServer((req, res) => {
+    if (req.url === "/api/products" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(products));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Rout not Found" }));
+    }
+  })
   .listen(`${PORT}`, () => {
     console.log(`Server run on Port ${PORT}`);
   });
