00d5942839fb14192cd5895414ce4d4f05d297aa
omidafifi <afifiomid@yahoo.com>
2026-03-27 23:05:26 +0330

    fs modules

--- miniProject2/server2.js
+++ miniProject2/server2.js
@@ -1,26 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
-const server = http
-  .createServer((req, res) => {
-    if (req.url === "/") {
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Home Page" }));
+  } else if (req.url === "/products") {
+    fs.readFile("./products.json", "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Server Error" }));
+        return;
+      }
+
       res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Wellcome" }));
-    } else if (req.url === "/about") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "About Page" }));
-    } else {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Not Found" }));
-    }
-  })
-  .listen(`${PORT}`, () => {
-    console.log(`Server run on Port ${PORT}`);
-  });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Not Found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server run on Port ${PORT}`);
+});
 
 
 
-  
 // res.end({ message: "Hi Omid" });
-// const products = require("./products.json");
 // res.writeHead(200, { "Content-Type": "application/json" });
 // res.end(JSON.stringify(products));
